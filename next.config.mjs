3f330c9eab5226de740dcfd20b2224394b8544b3
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-09-28 15:00:38 -0400

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["festive-camel-385.convex.cloud"], // Add the external domain here
+    domains: ["festive-camel-385.convex.cloud", "fine-husky-263.convex.cloud"], // Add the external domain here
   },
 };
 
